8d35d86294b382ab8cee0b126a959ba9d2e2285d
BodyaVan <bodya.van@gmail.com>
2015-03-19 02:16:20 +0300

    change context configuration in AppTests

--- src/test/java/ua/web_challenge/coffee_tea_shop/AppTests.java
+++ src/test/java/ua/web_challenge/coffee_tea_shop/AppTests.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -11,13 +12,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/dispatcher-servlet.xml")
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/application-context.xml")
 public class AppTests {
     private MockMvc mockMvc;
 
@@ -31,8 +31,8 @@ public class AppTests {
     }
 
     @Test
-    public void simple() throws Exception {
-        mockMvc.perform(get("/"))
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("hello"));
     }
