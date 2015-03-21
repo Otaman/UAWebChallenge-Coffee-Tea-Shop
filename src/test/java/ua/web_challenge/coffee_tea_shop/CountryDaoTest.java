1c5f4fd8f6b394a6336be89ca195973b24f33533
BodyaVan <bodya.van@gmail.com>
2015-03-21 19:52:27 +0300

    add tests for testing CountriesDao methods: find by id, find by name, insert, update, delete

--- src/test/java/ua/web_challenge/coffee_tea_shop/CountryDaoTest.java
+++ src/test/java/ua/web_challenge/coffee_tea_shop/CountryDaoTest.java
@@ -3,9 +3,13 @@ package ua.web_challenge.coffee_tea_shop;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,8 +23,11 @@ import ua.web_challenge.coffee_tea_shop.persistence.CountryDao;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.github.springtestdbunit.assertion.DatabaseAssertionMode.NON_STRICT;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.test.annotation.DirtiesContext.*;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.*;
 
 /**
  * Created on 17.03.2015
@@ -35,31 +42,15 @@ import static org.hamcrest.Matchers.*;
 @TestExecutionListeners({
         DependencyInjectionTestExecutionListener.class,
         DirtiesContextTestExecutionListener.class,
-        TransactionDbUnitTestExecutionListener.class,
         DbUnitTestExecutionListener.class})
-@Transactional
-@TransactionConfiguration(transactionManager = "transactionManager")
 @DatabaseSetup("/datasets/country-data.xml")
 public class CountryDaoTest {
     @Autowired
     private CountryDao countryDao;
 
     @Test
-    public void addCountry() throws Exception {
-        Country country = new Country();
-        country.setName("Spain");
-
-        countryDao.add(country);
-
-        List<Country> countries = countryDao.findAll();
-        List<String> countryNames = getCountryNames(countries);
-
-        assertThat(countryNames, is(hasItem("Spain")));
-    }
-
-    @Test
     public void findAll() throws Exception {
-        final List<Country> countries = countryDao.findAll();
+        List<Country> countries = countryDao.findAll();
         List<String> countryNames = getCountryNames(countries);
 
         assertThat(countryNames, is(containsInAnyOrder(
@@ -74,5 +65,43 @@ public class CountryDaoTest {
         return countries.stream()
                 .map(Country::getName)
                 .collect(Collectors.toList());
+    }
+
+    @Test
+    public void findById() throws Exception {
+        Country country = countryDao.findById(1);
+
+        assertThat(country.getName(), is("Ukraine"));
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/datasets/country-data-after-insert.xml", assertionMode = NON_STRICT)
+    public void addCountry() throws Exception {
+        Country country = new Country();
+        country.setName("Spain");
+
+        countryDao.add(country);
+    }
+
+    @Test
+    public void findByName() throws Exception {
+        Country country = countryDao.findByName("Ukraine");
+
+        assertThat(country.getName(), is("Ukraine"));
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/datasets/country-data-after-update.xml", assertionMode = NON_STRICT)
+    public void updateCountry() throws Exception {
+        Country country = countryDao.findById(4);
+        country.setName("Greece");
+        countryDao.update(country);
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/datasets/country-data-after-delete.xml", assertionMode = NON_STRICT)
+    public void deleteCountry() throws Exception {
+        Country country = countryDao.findById(4);
+        countryDao.delete(country);
     }
 }
